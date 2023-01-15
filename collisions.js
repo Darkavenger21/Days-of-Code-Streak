5ae9398787763c2bd1c0488949d9d1b3496f4005
Aarush <95063487+Darkavenger21@users.noreply.github.com>
2023-01-15 21:53:56 +0530

    Add files via upload

---
+++ collisions.js
@@ -0,0 +1,42 @@
+const collisionlevel1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 292, 292, 292, 292, 292, 292, 292, 292, 292, 292, 292, 292, 292, 292, 0,
+    0, 292, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 292, 0,
+    0, 292, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 292, 0,
+    0, 292, 292, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 292, 0,
+    0, 292, 292, 292, 292, 292, 292, 292, 292, 292, 292, 292, 292, 292, 292, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+Array.prototype.prase2D = function(){
+    
+    const rows =[]
+    for (let i=0; i<this.length; i+=16){
+        rows.push(this.slice(i, i+16))
+    }
+
+    return rows
+}
+
+
+const collisionBlocks = []          //creating collision block array
+
+const parsedCollisions = collisionlevel1.prase2D()
+
+parsedCollisions.forEach((row, y) => {
+    row.forEach((symbol, x) => {
+        if (symbol===292){
+            //pushing a new collision block in array
+            collisionBlocks.push(new CollisionBlock({
+                    position:{
+                        x: x * 64,
+                        y: y * 64,
+                    },
+                })
+            )
+        }
+    })
+})
+
+
+    
